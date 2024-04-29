2d4eaf09b0e928a38d77ddf0ccf037ee5048184a
sachin09060 <sachinsacchi734@gmail.com>
2024-04-29 23:24:58 +0530

    'arrays'

--- demo/ArrayOperations.java
+++ demo/ArrayOperations.java
@@ -4,143 +4,329 @@ import java.util.Scanner;
 public class ArrayOperations {
 	public static void main(String[] args) {
 		ArrayOperations array = new ArrayOperations();
-		ArrayOperations arr = new ArrayOperations();
-		arr.createArray();
-		arr.printArray();
-		arr.reverseArray();
-		arr.sortArray();
-		arr.mergeArray();
-		arr.zigZagMergeArray();
-		arr.maxElement();
-		arr.minElement();
-		arr.unionArray();
-		arr.intersection();
+		array.createArray();
+		array.printArray();
+		array.reverseArray(array);
+		array.sortArray();
+		array.mergeArray();
+		array.zigZagMergeArray();
+		array.maxElement();
+		array.minElement();
+		array.unionArray();
+		array.intersection();
+		array.occurance();
+        array.removeDuplicatesArray();
+        array.duplicateElemetArray();
+        array.countPrimeImplecants();
 	}
-	
+
+//	creating array method
+
 	public int[] createArray() {
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("From Create Array Method");
+        System.out.print("Enter the length of the array: ");
+        int length = scanner.nextInt();
 
-        System.out.println("Enter array size:");
-        int size = scanner.nextInt();
+        array = new int[length];
 
-        int[] array = new int[size];
-
-        System.out.println("Enter the elements: ");
-
-        for (int i = 0; i < size; i++) {
-            array[i] = scanner.nextInt();
+        for (int i = 0; i < length; i++) {
+            int element = scanner.nextInt();
+            array[i] = element;
         }
 
-        System.out.println("Array created with size " + size + ":");
-        for (int i = 0; i < size; i++) {
-        	String array1 = array[i] + " ";
-        }
-		int[] array1 = {};
-		return array1 ;
-		
-	}
-	
-	public void printArray() {
-		
-		System.out.println();
-		
-        System.out.println("From Create Array Method");
-		
-        Scanner scanner = new Scanner(System.in);
+        return array;
+    }
 
-        System.out.println("Enter array size:");
-        int size = scanner.nextInt();
+//	printing array method
 
-        int[] array = new int[size];
-
-        System.out.println("Enter the elements: ");
-
-        for (int i = 0; i < size; i++) {
-            array[i] = scanner.nextInt();
-        }
-
-        System.out.println("Array created with size " + size + ":");
-        for (int i = 0; i < size; i++) {
+    public void printArray(int[] array) {
+        System.out.print("Array: ");
+        for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
-	}
+        System.out.println();
+    }
 	
-	public void reverseArray() {
-		
-		System.out.println();
-        System.out.println("From Reverse Array Method");
-		
-        int[] array = {1,2,3,4,5,6};
-        int[] reversedArray = {};
-        
-//        for(int i = 0; i < array.length; i++) {
-//        	reversedArray = reversedArray +  array[i];
-//        }
+//	reversing array from previous return method
 
+	public int[] reverseArray(int[] array) {
+		int length = array.length;
+		for (int i = 0; i < length / 2; i++) {
+			int array2 = array[i];
+			array[i] = array[length - i - 1];
+			array[length - i - 1] = array2;
+		}
+		return array;
 	}
 	
-	public int[] sortArray() {
-		return null;	
+//	sorting array method
+
+public void sorting() {
+    int[] array = {1, 2, 3, 4, 5, 5, 5, 7, 8, 7, 8};
+    System.out.println("Sorted Array:");
+    
+    for (int i = 0; i < array.length - 1; i++) {
+        for (int j = 0; j < array.length - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    
+    for (int num : array) {
+        System.out.print(num + " ");
+    }
+    System.out.println();
+}
+
+// merging array method
+
+	public void mergeArray() {
+		int[] array1 = {1, 2, 3};
+		int[] array2 = {4, 5, 6};
+	
+		int mergedLength = array1.length + array2.length;
+		int[] mergedArray = new int[mergedLength];
+	
+		for (int i = 0; i < array1.length; i++) {
+			mergedArray[i] = array1[i];
+		}
+	
+		for (int i = 0; i < array2.length; i++) {
+			mergedArray[array1.length + i] = array2[i];
+		}
+
+		System.out.print("Merged Array: ");
+		for (int num : mergedArray) {
+			System.out.print(num + " ");
+		}
+		System.out.println();
 	}
 	
-	public int[] mergeArray() {
-		return null;
-	}
+//	zigzag array method
 	
-	public int[] zigZagMergeArray() {
-		return null;
-	}
-	
-	public int[] maxElement() {
-		return null;
-	}
-	
-	public int[] minElement() {
-		return null;
-	}
+	public void zigZagMergeArray() {
+
+        int[] array1 = {1, 2, 3};
+        int[] array2 = {4, 5, 6};
+
+        int maxLength = array1.length > array2.length ? array1.length : array2.length;
+
+        int[] mergedArray = new int[array1.length + array2.length];
+        int mergedIndex = 0;
+
+        for (int i = 0; i < maxLength; i++) {
+            if (i < array1.length) {
+                mergedArray[mergedIndex] = array1[i];
+                mergedIndex++;
+            }
+
+            if (i < array2.length) {
+                mergedArray[mergedIndex] = array2[i];
+                mergedIndex++;
+            }
+        }
+
+        System.out.print("Merged Array: ");
+        for (int num : mergedArray) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+// maximum element array method
+
+    public int maxElement() {
+        int[] array1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        if (array1.length == 0) {
+            return -1;
+        }
+
+        int max = array1[0];
+        for (int i = 1; i < array1.length; i++) {
+            if (array1[i] > max) {
+                max = array1[i];
+            }
+        }
+        return max;
+    }
+
+// minimum element array method
+
+    public int minElement() {
+        int[] array1 = {1, 2, 3, 4, 5, 6, 7, 8, 9}; 
+        if (array1.length == 0) {
+            return -1;
+        }
+
+        int min = array1[0];
+        for (int i = 1; i < array1.length; i++) {
+            if (array1[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+// union array method
 	
 	public int[] unionArray() {
-		
 		System.out.println();
 		System.out.println("From Union");
-		
-		int[] array = {1,2,3,4,5};
-		int[] array2 = {5,6,7,8};
-		int[] array3 = new int[array.length + array2.length];
-		
+	
+		int[] array1 = {1, 2, 3, 4, 5};
+		int[] array2 = {5, 6, 7, 8};
+		int[] array3 = new int[array1.length + array2.length];
+	
 		int i = 0, j = 0, k = 0;
-		
-		while(i< array.length && j< array2.length) {
-			if(array[i] == array2[j]) {
-				array3[k++] = array[i++];
+	
+		while (i < array1.length && j < array2.length) {
+			if (array1[i] < array2[j]) {
+				array3[k++] = array1[i++];
+			} else if (array1[i] > array2[j]) {
+				array3[k++] = array2[j++];
+			} else {
+				array3[k++] = array1[i++];
 				j++;
-				
-			}else if(array[i]<array2[j]) {
-				array3[k++]=array2[j++];
 			}
-			
 		}
-		while(i<array.length) {
-			array3[k++] = array[i++];
+
+		while (i < array1.length) {
+			array3[k++] = array1[i++];
 		}
-		
-		while(j<array2.length) {
+	
+		while (j < array2.length) {
 			array3[k++] = array2[j++];
 		}
-		
-		for(int value: array3) {
-			if(i!=0) {
-				System.out.println(value);
+
+		System.out.println("Union Array:");
+		for (int l = 0; l < k; l++) {
+			System.out.println(array3[l]);
+		}
+		int[] unionArray = Arrays.copyOf(array3, k);
+
+		return unionArray;
+	}
+
+//	intersection array method
+
+	public void intersectionArray() {
+		int[] array1 = {1, 2, 3, 4, 5};
+		int[] array2 = {5, 6, 7, 8};
+	
+		for (int i = 0; i < array1.length - 1; i++) {
+			for (int j = 0; j < array1.length - i - 1; j++) {
+				if (array1[j] > array1[j + 1]) {
+					int temp = array1[j];
+					array1[j] = array1[j + 1];
+					array1[j + 1] = temp;
+				}
 			}
 		}
-		
-		return array3;
+	
+		for (int i = 0; i < array2.length - 1; i++) {
+			for (int j = 0; j < array2.length - i - 1; j++) {
+				if (array2[j] > array2[j + 1]) {
+					int temp = array2[j];
+					array2[j] = array2[j + 1];
+					array2[j + 1] = temp;
+				}
+			}
+		}
+	
+		int maxLength = Math.min(array1.length, array2.length);
+		System.out.println("Intersection of arrays:");
+		for (int i = 0, j = 0; i < array1.length && j < array2.length;) {
+			if (array1[i] == array2[j]) {
+				System.out.print(array1[i] + " ");
+				i++;
+				j++;
+			} else if (array1[i] < array2[j]) {
+				i++;
+			} else {
+				j++;
+			}
+		}
+		System.out.println();
 	}
 	
-	public int[] intersection() {
-		return null;
+// occurance array method
+	
+	public void occurance() {
+		int[] array1 = {1, 2, 3, 4, 5, 5, 5, 7, 8, 7, 8};
+		System.out.println("Occurrence of each element:");
+		Arrays.sort(array1);
+		int current = array1[0];
+		int count = 1;
+	
+		for (int i = 1; i < array1.length; i++) {
+			if (array1[i] == current) {
+				count++;
+			} else {
+				System.out.println(current + ": " + count);
+				current = array1[i];
+				count = 1;
+			}
+		}
+
+		System.out.println(current + ": " + count);
 	}
+
+// remove duplicate array mmethod
+
+	public void removeDuplicatesArray() {
+		int[] array1 = {1, 2, 3, 4, 5, 5, 5, 7, 8, 7, 8};
+		System.out.println("Array after removing duplicates:");
+		int length = array1.length;
+	
+		for (int i = 0; i < length - 1; i++) {
+			for (int j = i + 1; j < length; j++) {
+				if (array1[i] == array1[j]) {
+					for (int k = j; k < length - 1; k++) {
+						array1[k] = array1[k + 1];
+					}
+					length--;
+					j--;
+				}
+			}
+		}
+	
+		for (int i = 0; i < length; i++) {
+			System.out.print(array1[i] + " ");
+		}
+		System.out.println();
+	}
+
+// print duplicate vale array method
+
+	public void duplicateElemetArray() {
+		int[] array1 = {1, 2, 3, 4, 5, 5, 5, 7, 8, 7, 8};
+		System.out.println("Duplicate Elements:");
+		for (int i = 0; i < array1.length - 1; i++) {
+			for (int j = i + 1; j < array1.length; j++) {
+				if (array1[i] == array1[j]) {
+					System.out.print(array1[i] + " ");
+					break;
+				}
+			}
+		}
+		System.out.println();
+	}
+
+//	count prime array method
+
+	public void countPrimeImplecants() {
+		int[] array1 = {1, 2, 3, 4, 5, 5, 5, 7, 8, 7, 8};
+		System.out.println("Prime Elements:");
+		for (int num : array1) {
+			if (isPrime(num)) {
+				System.out.print(num + " ");
+			}
+		}
+		System.out.println();
+	}
+		
 	
 }
