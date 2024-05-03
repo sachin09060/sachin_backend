38ced373a4fb64e85b44eb38c18b1c57e869d7b4
sachin09060 <sachinsacchi734@gmail.com>
2024-05-03 13:10:34 +0530

    'Day4'

---
+++ Day4/src/set/Register.java
@@ -0,0 +1,106 @@
+package set;
+import java.util.*;
+
+public class Register {
+	
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String phone;
+    
+    public Register() {
+		super();
+	}
+    
+    public Register(Integer id, String name, Integer age, String phone) {
+    	super();
+    	this.id = id;
+    	this.name = name;
+    	this.age = age;
+    	this.phone= phone;
+    }
+    
+    public Integer getId() {
+    	return id;
+    }
+    
+    public void setId(int id) {
+    	this.id = id;
+    }
+    
+    public String getName() {
+    	return name;
+    }
+    
+    public void setName(String name) {
+    	this.name = name;
+    }
+    
+    public Integer getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public String getPhone() {
+        return phone;
+    }
+    
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return this.id+" "+this.name+" "+this.age+" "+this.phone+" ";
+    }
+    
+
+	public static void main(String[] args) {
+		
+
+		Scanner scanner = new Scanner(System.in);
+		Comparator<Register> com = new Comparator<Register>() {
+			
+			@Override
+			public int compare(Register o1, Register o2) {
+				return (o1.id).compareTo(o2.id);
+			}
+		};
+		
+		TreeSet<Register> register = new TreeSet<Register>(com);
+		
+        while(true) {
+            System.out.println("Enter id to Register: ");
+            int id = scanner.nextInt();
+            
+            System.out.println("Enter Name to Register: ");
+            String name = scanner.next();
+            
+            System.out.println("Enter Age to Register: ");
+            int age = scanner.nextInt();
+            
+            System.out.println("Enter Phone number to Register: ");
+            String phone = scanner.next();
+            
+            System.out.println("Do you want to add some more ?");
+            String data = scanner.next();
+
+            register.add(new Register(id, name, age, phone));
+            if(!data.equalsIgnoreCase("yes"))
+                break;
+        }
+        
+        Iterator<Register> itr = register.iterator();
+        while(itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+        
+        scanner.close();
+
+	}
+
+}
---
+++ Day4/src/set/Student.java
@@ -0,0 +1,103 @@
+package set;
+import java.util.*;
+
+public class Student {
+
+    private Integer sId;
+    private String name;
+    private Integer age;
+    private Integer marks;
+
+    public Student() {
+        super();
+    }
+    
+    public Student(Integer sId, String name, Integer age, Integer marks) {
+        super();
+        this.sId = sId;
+        this.name = name;
+        this.age = age;
+        this.marks = marks;
+    }
+    
+    public Integer geteId() {
+        return sId;
+    }
+    
+    public void setSId(int sId) {
+        this.sId = sId;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public Integer getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public Integer getMarks() {
+        return marks;
+    }
+    
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+    
+    @Override
+    public int hashCode() {
+    	return this.age;
+    }
+    
+    @Override
+    public boolean equals(Object e) {
+    	Student students = (Student)e;
+		return this.name == students.name && this.marks == students.marks;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return this.sId+" "+this.name+" "+this.age+" "+this.marks+" ";
+    }
+    
+    public static void main(String[] args) {
+        Set<Student> students = new HashSet<>();
+        Scanner scanner = new Scanner(System.in);
+        
+        while(true) {
+            System.out.println("Enter SID of Student: ");
+            int sID = scanner.nextInt();
+            System.out.println("Enter Name of Student: ");
+            String name = scanner.next();
+            System.out.println("Enter Age of Student: ");
+            int age = scanner.nextInt();
+            
+            System.out.println("Enter Marks of Student: ");
+            int marks = scanner.nextInt();
+            
+            System.out.println("Do you want to add some more students ?");
+            String data = scanner.next();
+
+            students.add(new Student(sID, name, age, marks));
+            if(!data.equalsIgnoreCase("yes"))
+                break;
+        }
+        
+        Iterator<Student> itr = students.iterator();
+        while(itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+        
+        scanner.close();
+    }
+
+}
