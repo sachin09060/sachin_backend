7b7479baf170fd30a2257511db408efcdb771c37
sachin09060 <sachinsacchi734@gmail.com>
2024-05-08 16:27:13 +0530

    Adding sort operation for merged array

--- Day1/src/arrayOperations/MergeArray.java
+++ Day1/src/arrayOperations/MergeArray.java
@@ -9,8 +9,8 @@ public class MergeArray {
 	}
 	
 	public void mergeArray() {
-		int[] array1 = {1, 2, 3};
-		int[] array2 = {4, 5, 6};
+		int[] array1 = {1, 2,1000, 3};
+		int[] array2 = {4, 5, 6, 400, 200, 100};
 	
 		int mergedLength = array1.length + array2.length;
 		int[] mergedArray = new int[mergedLength];
@@ -23,11 +23,20 @@ public class MergeArray {
 			mergedArray[array1.length + i] = array2[i];
 		}
 
-		System.out.print("Merged Array: ");
+		System.out.print("Merged Array is sorted now: ");
 		for (int num : mergedArray) {
+			
+		    for (int i = 0; i < mergedArray.length - 1; i++) {
+		        for (int j = 0; j < mergedArray.length - i - 1; j++) {
+		            if (mergedArray[j] > mergedArray[j + 1]) {
+		                int num2 = mergedArray[j];
+		                mergedArray[j] = mergedArray[j + 1];
+		                mergedArray[j + 1] = num2;
+		            }
+		        }
+		    }
+		    
 			System.out.print(num + " ");
 		}
-		System.out.println();
 	}
-
 }
