550a598eba00ced5a95559f00d40f865362e771d
sachin09060 <sachinsacchi734@gmail.com>
2024-05-21 18:01:30 +0530

    Done

--- libraryManagementSystem/src/main/java/com/excel/libraryManagementSystem/entity/BookHistory.java
+++ libraryManagementSystem/src/main/java/com/excel/libraryManagementSystem/entity/BookHistory.java
@@ -2,8 +2,10 @@ package com.excel.libraryManagementSystem.entity;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -45,11 +47,11 @@ public class BookHistory {
     @Column(name = "is_renewed")
     private Boolean renewed;
     
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
     
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     @JoinColumn(name = "book_id")
     private Book book;
 }
